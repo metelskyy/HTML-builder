b3a7a906edd7e90c95f00f2df4926a6b1f548ef1
metelskyy <youngkerik@gmail.com>
2022-05-24 22:57:38 +0300

    fix 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,30 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, () => {});
-fs.readdir(path.join(__dirname, "files"), (err, data) => {
-  if (err) console.log(err);
-  data.forEach((file) => {
-    fs.copyFile(
-      path.join(__dirname, "files", file),
-      path.join(__dirname, "files-copy", file),
-      (err) => {
-        if (err) console.log(err);
-      }
-    );
+const copyDir = async () => {
+  await fs.mkdir(
+    path.join(__dirname, "files-copy"),
+    { recursive: true },
+    () => {}
+  );
+
+  await fs.readdir(path.join(__dirname, "files-copy"), (err, data) => {
+    data.forEach((file) => {
+      fs.unlink(path.join(__dirname, "files-copy", file), () => {});
+    });
   });
-});
+
+  fs.readdir(path.join(__dirname, "files"), (err, data) => {
+    if (err) console.log(err);
+    data.forEach((file) => {
+      fs.copyFile(
+        path.join(__dirname, "files", file),
+        path.join(__dirname, "files-copy", file),
+        (err) => {
+          if (err) console.log(err);
+        }
+      );
+    });
+  });
+};
+copyDir();
